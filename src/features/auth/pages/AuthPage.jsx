4b63ce8d39fe9614f7a009cac8e98bb2054ef2e1
Khang-Pham-jh <khang.pham@journeyh.io>
2026-06-02 17:35:30 +0700

    Update atuh routing

--- src/features/auth/pages/AuthPage.jsx
+++ src/features/auth/pages/AuthPage.jsx
@@ -1,13 +1,8 @@
 import { Link, Outlet } from 'react-router-dom';
-
+import styles from './Authpage.module.css';
 export default function AuthPage() {
   return (
-    <main>
-      <div style={{ display: 'flex', gap: 8, marginBottom: 18 }}>
-        <Link to="/login">Login</Link>
-        <Link to="/signup">Sign up</Link>
-      </div>
-
+    <main className={styles.authPageContainer}>
       <Outlet />
     </main>
   );
